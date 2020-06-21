c762c9779d54de791e68a8a20ec0c986b0623394
MachineLearning2020-ai <67120256+MachineLearning2020-ai@users.noreply.github.com>
2020-06-21 17:38:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 from flask import Flask,request,jsonify,render_template
 import joblib
-server=Flask(__name__)
+app=Flask(__name__)
 model=joblib.load("bmi.pkl")
-@server.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@server.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -18,14 +18,14 @@ def predict():
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
-    print(output)
+    #print(output)
     index_target=pd.Series(["Extremely Weak","Weak" ,"Normal" ,"Overweight","Obesity" ,"Extreme Obesity"])
     result=index_target[output]
     #result=list(result.values)
     #result=str(result)
     return render_template('index.html', prediction_text='Predicted BMI  {}'.format(result))
 
-@server.route('/predict_api',methods=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
